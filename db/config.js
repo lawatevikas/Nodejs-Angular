39b6cd5985a707b2f1d174b5716843a59eef143d
vikas <lawatevikas809@gmail.com>
2023-08-08 22:59:49 +0530

    Database connection

--- db/config.js
+++ db/config.js
@@ -1,2 +1,5 @@
 const mongoose=require('mongoose')
-mongoose.connect("mongodb://localhost:27017/New-E-comm")
+//mongoose.connect("mongodb://localhost:27017/New-E-comm")  
+mongoose.connect("mongodb+srv://lawatevikas809:m4Wk2nbWzHYKDK5V@cluster0.snosd4k.mongodb.net/E-comm?retryWrites=true&w=majority")
+
+
